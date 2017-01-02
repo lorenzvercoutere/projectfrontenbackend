7c556f0b22f33b75aa33f1eb8a2867ee3d2eb79a
Gurbii <gerben.rosseel@student.howest.be>
2017-01-02 15:10:28 +0100

    Basis chat

---
+++ server.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Student on 02-Jan-17.
+ */
+var static = require("node-static"),
+    config = require("./config.js"),
+    port = process.env.port || config.PORT;
+
+var fileServer = new static.Server('./public', {
+    cache: 0,
+    gzip: true
+});
+
+var app = require('http').createServer(function (req, res) {
+    req.addListener('end', function () {
+        fileServer.serve(req, res);
+    }).resume();
+});
+
+
+app.listen(port);
+
+var io = require("socket.io").listen(app);
+console.log("server en sockets luisteren naar poort: " + port);
+
+require("./js/sockets.js")(io);
---
+++ wwwroot/js/sockets.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Student on 02-Jan-17.
+ */
